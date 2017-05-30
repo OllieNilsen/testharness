f94b8c84b6992bc9bb48d90ff89e5f603a05555f
ollie <ollie@jigsaw.xyz>
2017-05-30 12:51:52 +0100

    tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 const sinonChai = require('sinon-chai');
 const chai = require('chai');
 const should = chai.should();
-const sut = require('../src');
+const sut = require('../src/cartesian');
 chai.use(sinonChai);
 
 describe('index', () => {
@@ -10,15 +10,72 @@ describe('index', () => {
   });
 
   describe('takeNext', () => {
-    it('yyy', () => {
-      generators = {
-        list: sut.list('list', [1, 2, 3]),
-        boole: sut.boolean('boole'),
-        num: sut.numeric('num', 0, 10, 2)
-      }
-      const carteser = sut.lazyCartesianProduct(generators);
-      sut.takeNext(2, carteser).should.eql([1, 2, 3])
+    let cartesian, sources;
+    beforeEach(() => {
+      sources = new sut.Sources();
+      sources.list('something', [1, 2])
+      sources.boolean('lit');
+      sources.numeric('age', { min: 24, max: 25, step: 1 });
+      cartesian = sut.lazyCartesian(sources);
+    });
+    // afterEach(() => {
+    //   cartesian = undefined;
+    // })
+    it('should yield all possible combinations of the sources', () => {
+
+      sut.takeNext(8, cartesian).should.eql([
+        {
+          "age": 24,
+          "something": 1,
+          "lit": true
+        },
+        {
+          "age": 24,
+          "something": 2,
+          "lit": true
+        },
+        {
+          "age": 24,
+          "something": 1,
+          "lit": false
+        },
+        {
+          "age": 24,
+          "something": 2,
+          "lit": false
+        },
+        {
+          "age": 25,
+          "something": 1,
+          "lit": true
+        },
+        {
+          "age": 25,
+          "something": 2,
+          "lit": true
+        },
+        {
+          "age": 25,
+          "something": 1,
+          "lit": false
+        },
+        {
+          "age": 25,
+          "something": 2,
+          "lit": false
+        }
+      ]);
+
+    });
+
+    it('should stop when it has yielded all possible combinations', () => {
+      sut.takeNext(8, cartesian).length.should.equal(8);
+      sut.takeNext(16, cartesian).length.should.equal(0);
+    });
+
+    it('should not return more combinations than the number of possible combinations', () => {
+      sut.takeNext(10, cartesian).length.should.equal(8);
     });
   });
-});
 
+});
